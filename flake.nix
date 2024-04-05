22f66545a3b6b8875ddab560567af3e4f5984d05
Murtaza Udaipurwala <murtaza@murtazau.xyz>
2024-04-05 16:30:11 +0530

    ci: update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,35 +1,38 @@
 {
   description = "Murtaza Udaipurwala's Go Monolith";
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
-  outputs = { self, nixpkgs, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      formatter.${system} = pkgs.nixpkgs-fmt;
-      packages.${system} = {
-        default = pkgs.buildGoModule {
-          pname = "z";
-          version = "0.2.0";
-          src = ./.;
-          vendorSha256 = "sha256-U5c5jagaRW+lq0jsxWnngdcU2YYEl8Jn2Phuuq5vdzs=";
-          CGO_ENABLED = 0;
-          subPackages = [ "cmd/z" ];
-          nativeBuildInputs = [ pkgs.installShellFiles ];
-          postInstall = ''
-            for shell in bash zsh; do
-              installShellCompletion --$shell ./completion/$shell/z
-            done
-          '';
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    flake-utils.url = "github:numtide/flake-utils/v1.0.0";
+  };
+  outputs = { self, nixpkgs, flake-utils, ... }@inputs:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        formatter = pkgs.nixpkgs-fmt;
+        packages = {
+          default = pkgs.buildGoModule {
+            pname = "z";
+            version = "0.2.0";
+            src = ./.;
+            vendorSha256 = "sha256-U5c5jagaRW+lq0jsxWnngdcU2YYEl8Jn2Phuuq5vdzs=";
+            CGO_ENABLED = 0;
+            subPackages = [ "cmd/z" ];
+            nativeBuildInputs = [ pkgs.installShellFiles ];
+            postInstall = ''
+              for shell in bash zsh; do
+                installShellCompletion --$shell ./completion/$shell/z
+              done
+            '';
+          };
         };
-      };
-      devShells.${system}.default = pkgs.mkShell {
-        packages = with pkgs; [
-          go
-          go-tools
-          gopls
-        ];
-      };
-    };
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            go
+            go-tools
+            gopls
+          ];
+        };
+      });
 }
